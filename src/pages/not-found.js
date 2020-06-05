7dce905d252a52bf55dbe4d301e6d9a2c3760b80
Graham Colmer <grahamcolmer@grahams-mbp.home>
2020-06-05 14:55:30 +0100

    Not found page

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Layout from '../components/shared/Layout';
+import { Typography } from "@material-ui/core";
 
 function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  return (
+    <Layout title="Page Not Found" marginTop={120}>
+      <Typography variant="h5" align="center" paragraph>
+        Sorry, this page isn't available
+      </Typography>
+    </Layout>
+  )
 }
 
 export default NotFoundPage;
